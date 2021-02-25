c0df7976ad494984adfd24fa38fa4e618a9931ad
eichmann <david-eichmann@uiowa.edu>
2021-02-25 22:30:09 +0000

    Some updates to the harvester to support Julie's roster request.

--- src/main/java/org/cd2h/slack/util/Harvester.java
+++ src/main/java/org/cd2h/slack/util/Harvester.java
@@ -27,76 +27,90 @@ public class Harvester extends SlackAPI {
     static String midfix = "?token=";
     static String suffix = "&pretty=1";
     
-    static public void main(String[] args) throws ClassNotFoundException, SQLException {
-	PropertyConfigurator.configure(args[0]);
-	prop_file = PropertyLoader.loadProperties("slack");
-	conn = getConnection();
-	
-	switch (args[1]) {
-	case "channels":
-	    fetchChannels();
-	    break;
-	case "users":
-	    fetchUsers();
-	    break;
-	case "members":
-	    fetchMembers();
-	    break;
-	case "messages":
-	    fetchMessages();
-	    break;
-	default:
-	    break;
+	static public void main(String[] args) throws ClassNotFoundException, SQLException {
+		PropertyConfigurator.configure(args[0]);
+		prop_file = PropertyLoader.loadProperties("slack");
+		conn = getConnection();
+
+		switch (args[1]) {
+		case "channels":
+			fetchChannels();
+			break;
+		case "users":
+			fetchUsers();
+			break;
+		case "members":
+			fetchMembers();
+			break;
+		case "messages":
+			fetchMessages();
+			break;
+		default:
+			break;
+		}
 	}
-    }
     
-    public static void fetchChannels() throws SQLException {
-	JSONObject result = fetch("conversations.list","");
-	for (int i = 0; i < result.getJSONArray("channels").length(); i++) {
-	    JSONObject channel = result.getJSONArray("channels").getJSONObject(i);
-	    logger.info("channel: " + channel.toString(3));
-	    
-	    PreparedStatement stmt = conn.prepareStatement("insert into cd2h_slack.channel_raw values(?::jsonb)");
-	    stmt.setString(1, channel.toString(3));
-	    stmt.execute();
-	    stmt.close();
+	public static void fetchChannels() throws SQLException {
+		simpleStmt("truncate cd2h_slack.channel_raw");
+		JSONObject result = fetch("conversations.list", "");
+		for (int i = 0; i < result.getJSONArray("channels").length(); i++) {
+			JSONObject channel = result.getJSONArray("channels").getJSONObject(i);
+			logger.info("channel: " + channel.toString(3));
+
+			PreparedStatement stmt = conn.prepareStatement("insert into cd2h_slack.channel_raw values(?::jsonb)");
+			stmt.setString(1, channel.toString(3));
+			stmt.execute();
+			stmt.close();
+		}
+		simpleStmt("refresh materialized view cd2h_slack.channel");
+
 	}
 
-    }
+	public static void fetchUsers() throws SQLException {
+		simpleStmt("truncate cd2h_slack.user_raw");
+		String cursor = null;
+		do {
+			JSONObject result = fetch("users.list", "&limit=1000" + (cursor == null ? "" : "&cursor=" + cursor));
+			for (int i = 0; i < result.getJSONArray("members").length(); i++) {
+				JSONObject user = result.getJSONArray("members").getJSONObject(i);
+				logger.info("user: " + user.toString(3));
 
-    public static void fetchUsers() throws SQLException {
-	JSONObject result = fetch("users.list","");
-	for (int i = 0; i < result.getJSONArray("members").length(); i++) {
-	    JSONObject user = result.getJSONArray("members").getJSONObject(i);
-	    logger.info("user: " + user.toString(3));
-	    
-	    PreparedStatement stmt = conn.prepareStatement("insert into cd2h_slack.user_raw values(?::jsonb)");
-	    stmt.setString(1, user.toString(3));
-	    stmt.execute();
-	    stmt.close();
+				PreparedStatement stmt = conn.prepareStatement("insert into cd2h_slack.user_raw values(?::jsonb)");
+				stmt.setString(1, user.toString(3));
+				stmt.execute();
+				stmt.close();
+			}
+			JSONObject response = result.optJSONObject("response_metadata");
+			cursor = response == null ? null : response.optString("next_cursor");
+			logger.info("cursor: " + cursor);
+		} while (cursor != null && cursor.length() > 0);
+		simpleStmt("refresh materialized view cd2h_slack.person");
 	}
 
-    }
+	public static void fetchMembers() throws SQLException {
+		simpleStmt("truncate cd2h_slack.member");
+		PreparedStatement fetchStmt = conn.prepareStatement("select id from cd2h_slack.channel where not archived");
+		ResultSet fetchRS = fetchStmt.executeQuery();
+		while (fetchRS.next()) {
+			String channelID = fetchRS.getString(1);
+			try {
+				JSONObject result = fetch("conversations.members", "&channel=" + channelID + "&limit=1000");
+				for (int i = 0; i < result.getJSONArray("members").length(); i++) {
+					String user = result.getJSONArray("members").getString(i);
+					logger.info("\tuser: " + user);
 
-    public static void fetchMembers() throws SQLException {
-	PreparedStatement fetchStmt = conn.prepareStatement("select id from cd2h_slack.channel where not archived");
-	ResultSet fetchRS = fetchStmt.executeQuery();
-	while (fetchRS.next()) {
-	    String channelID = fetchRS.getString(1);
-	JSONObject result = fetch("conversations.members","&channel="+channelID+"&limit=1000");
-	for (int i = 0; i < result.getJSONArray("members").length(); i++) {
-	    String user = result.getJSONArray("members").getString(i);
-	    logger.info("\tuser: " + user);
-	    
-	    PreparedStatement stmt = conn.prepareStatement("insert into cd2h_slack.member values(?,?)");
-	    stmt.setString(1, channelID);
-	    stmt.setString(2, user);
-	    stmt.execute();
-	    stmt.close();
+					PreparedStatement stmt = conn.prepareStatement("insert into cd2h_slack.member values(?,?)");
+					stmt.setString(1, channelID);
+					stmt.setString(2, user);
+					stmt.execute();
+					stmt.close();
+				}
+			} catch (Exception e) {
+				logger.error("error raised fetching members:", e);
+			}
+		}
+		fetchStmt.close();
 	}
-	}
-	fetchStmt.close();
-    }
 
     public static void fetchMessages() throws SQLException {
     	PreparedStatement channelStmt = conn.prepareStatement("select channel.name from cd2h_slack.person,cd2h_slack.member,cd2h_slack.channel where person.id=member.user_id and member.channel_id=channel.id and display_name='analytics' order by name");
@@ -131,26 +145,38 @@ public class Harvester extends SlackAPI {
     	channelStmt.close();
     }
 
-    public static JSONObject fetch(String entity, String params) {
-	JSONObject container = null;;
-	try {
-	    URL theURL = new URL(prefix + entity + midfix + getOAuthToken() + suffix + params);
-	    BufferedReader reader = new BufferedReader(new InputStreamReader(theURL.openConnection().getInputStream()));
+	public static JSONObject fetch(String entity, String params) {
+		JSONObject container = null;
+		try {
+			URL theURL = new URL(prefix + entity + midfix + getOAuthToken() + suffix + params);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(theURL.openConnection().getInputStream()));
 
-	    container = new JSONObject(new JSONTokener(reader));
-	} catch (Exception e) {
-	    logger.error("exception:", e);
+			container = new JSONObject(new JSONTokener(reader));
+		} catch (Exception e) {
+			logger.error("exception:", e);
+		}
+		logger.info("container: " + container.toString(3));
+		return container;
 	}
-	logger.info("container: " + container.toString(3));
-	return container;
-    }
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
-	Class.forName("org.postgresql.Driver");
-	Properties props = new Properties();
-	props.setProperty("user", prop_file.getProperty("jdbc.user"));
-	props.setProperty("password", prop_file.getProperty("jdbc.password"));
-	Connection conn = DriverManager.getConnection(prop_file.getProperty("jdbc.url"), props);
-	return conn;
-    }
+	public static void simpleStmt(String queryString) {
+		try {
+			logger.info("executing " + queryString + "...");
+			PreparedStatement beginStmt = conn.prepareStatement(queryString);
+			beginStmt.executeUpdate();
+			beginStmt.close();
+		} catch (Exception e) {
+			logger.error("Error in database initialization: " + e);
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		Class.forName("org.postgresql.Driver");
+		Properties props = new Properties();
+		props.setProperty("user", prop_file.getProperty("jdbc.user"));
+		props.setProperty("password", prop_file.getProperty("jdbc.password"));
+		Connection conn = DriverManager.getConnection(prop_file.getProperty("jdbc.url"), props);
+		return conn;
+	}
 }
